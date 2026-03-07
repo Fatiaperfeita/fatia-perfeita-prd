6387ab57a110df21c104ebfe8af266fa1988f0a4
Abraão Gomes <94805151+Abraaog@users.noreply.github.com>
2026-03-07 14:31:36 -0300

    Delete app directory

--- app/frontend/src/services/api.js
+++
@@ -1,9 +0,0 @@
-import { gerarMockPedidos } from '../data/mockData';
-
-export const getPedidos = () => {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve(gerarMockPedidos());
-        }, 500); // Simulate network delay
-    });
-};
